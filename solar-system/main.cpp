48410f422b6cbb0a19e8fb996333f75c09a5e1d8
Luís Santos <luis.carlos.santos@ubi.pt>
2023-01-05 22:33:28 +0000

    initial commit

---
+++ solar-system/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  solar-system
+//
+//  Created by Luís Santos on 05/01/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
